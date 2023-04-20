e4f56dda02e81249577ca155a07acc20c2b47962
khuong-d4ng <khuongdang.pham@gmail.com>
2023-04-21 06:01:23 +0700

    code format

--- js/scripts.js
+++ js/scripts.js
@@ -1,41 +1,47 @@
-window.addEventListener("DOMContentLoaded", function() {
-    var anhCanXoaElements = document.querySelectorAll(".anh-can-xoa");
-    var divToResize = document.getElementsByClassName("anh-giu-lailai")[0];
-    var screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-  
-    function hideAnhCanXoaElements() {
-      for (var i = 0; i < anhCanXoaElements.length; i++) {
-        anhCanXoaElements[i].style.display = "none";
-      }
-      if (divToResize) {
-          divToResize.style.width = "100%";
-      }
+window.addEventListener("DOMContentLoaded", function () {
+  var anhCanXoaElements = document.querySelectorAll(".anh-can-xoa");
+  var divToResize = document.getElementsByClassName("anh-giu-lailai")[0];
+  var screenWidth =
+    window.innerWidth ||
+    document.documentElement.clientWidth ||
+    document.body.clientWidth;
+
+  function hideAnhCanXoaElements() {
+    for (var i = 0; i < anhCanXoaElements.length; i++) {
+      anhCanXoaElements[i].style.display = "none";
     }
-  
-    function showAnhCanXoaElements() {
-      for (var i = 0; i < anhCanXoaElements.length; i++) {
-        anhCanXoaElements[i].style.display = "";
-      }
-      if (divToResize) {
-          divToResize.style.width = "33.33%";
-      }
+    if (divToResize) {
+      divToResize.style.width = "100%";
     }
-  
-    function checkScreenWidth() {
-      screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
-      if (screenWidth < 550) {
-        hideAnhCanXoaElements();
-      } else {
-        showAnhCanXoaElements();
-      }
+  }
+
+  function showAnhCanXoaElements() {
+    for (var i = 0; i < anhCanXoaElements.length; i++) {
+      anhCanXoaElements[i].style.display = "";
     }
-  
+    if (divToResize) {
+      divToResize.style.width = "33.33%";
+    }
+  }
+
+  function checkScreenWidth() {
+    screenWidth =
+      window.innerWidth ||
+      document.documentElement.clientWidth ||
+      document.body.clientWidth;
+    if (screenWidth < 550) {
+      hideAnhCanXoaElements();
+    } else {
+      showAnhCanXoaElements();
+    }
+  }
+
+  checkScreenWidth();
+
+  window.addEventListener("resize", function () {
     checkScreenWidth();
-  
-    window.addEventListener("resize", function() {
-      checkScreenWidth();
-    });
   });
+});
 
 /** ĐOẠN NÀY ĐỂ CUỘN XUỐNG PHẦN HOT TOUUR KHI CLICK NÀO NEW! */
 
@@ -43,6 +49,6 @@ function smoothScroll(event) {
   event.preventDefault();
   const targetId = event.currentTarget.getAttribute("href");
   document.querySelector(targetId).scrollIntoView({
-    behavior: "smooth"
+    behavior: "smooth",
   });
 }
