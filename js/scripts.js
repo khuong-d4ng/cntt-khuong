50336aee33f799d94fa470cab63e0260f110653f
khuong-d4ng <khuongdang.pham@gmail.com>
2023-05-20 01:48:27 +0700

    lam dc qua nhieu thu, responsive dai khai da xong moi thu, lam dc them banner, stories, fix 1 so thu

--- js/scripts.js
+++ js/scripts.js
@@ -53,35 +53,118 @@ function smoothScroll(event) {
   });
 }
 
-
-
 /* ĐIỀU KIỆN CHO QUẢ RESPONSIVE ĐỈNH CON MẸ NÓ CAO: TRÊN 500PX THÌ HIỂN THỊ rSECTION HOME BÌNH THƯỜNG(BẢN DESKTOP) 
 DƯỚI < 500PX THÌ HIỂN THỊ BẢN SLIDER HAHAHAH NGONNNNNNNNNNNNN*/
+
+$("#rSection-slider").slick({
+  centerMode: true,
+  centerPadding: "60px",
+  slidesToShow: 1,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: "40px",
+        slidesToShow: 1,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: "40px",
+        slidesToShow: 1,
+      },
+    },
+  ],
+  arrows: false,
+});
+
+$(document).ready(function () {
+
+  var windowWidth = $(window).width();
+  var slideToShowNumber = 2;
+    var slideScrollNumber = 2;
+
+    if (windowWidth < 600) {
+      slideToShowNumber = 1;
+      slideScrollNumber = 1;
+    }
+    $("#stories-slider").slick({
+      infinite: true,
+        slidesToShow: slideScrollNumber,
+        slidesToScroll: slideToShowNumber,
+      prevArrow:
+        "<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+      nextArrow:
+        "<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
+    });
+  updateSlider();
+
+  $(window).on("resize", function () {
+    var newWindowWidth = $(window).width();
+    if (newWindowWidth !== windowWidth) {
+      windowWidth = newWindowWidth;
+      updateSlider();
+    }
+  });
+});
+$(document).ready(function () {
+  var slideToShowNumber = 2;
+  var slideScrollNumber = 2;
+
+  function updateSlider() {
+    var windowWidth = $(window).width();
+
+    if (windowWidth < 600) {
+      slideToShowNumber = 1;
+      slideScrollNumber = 1;
+    } else {
+      slideToShowNumber = 2;
+      slideScrollNumber = 2;
+    }
+
+    $("#stories-slider").slick({
+      infinite: true,
+      slidesToShow: slideToShowNumber,
+      slidesToScroll: slideScrollNumber,
+      prevArrow:
+        "<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+      nextArrow:
+        "<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
+    });
+  }
+
+  updateSlider();
+
+  $(window).on("resize", function () {
+    updateSlider();
+  });
+});
+
+
 window.addEventListener("DOMContentLoaded", function () {
-  var rightSectionHome = document.querySelector(".right-container-home");
-  var rSectionSlider = document.querySelector(".rSection-slider");
+  var rightContainerHome = document.querySelector(".right-container-home");
+  var rSectionSlider = document.querySelector("#rSection-slider");
 
-  function hideRightSectionHome() {
-    rightSectionHome.style.display = "none";
-    rSectionSlider.style.display = "block";
+  function updateLayout() {
+    var windowWidth = window.innerWidth;
+
+    if (windowWidth < 500) {
+      rightContainerHome.style.display = "none";
+      rSectionSlider.style.display = "block";
+    } else {
+      rightContainerHome.style.display = "block";
+      rSectionSlider.style.display = "none";
+    }
   }
 
-  function showRightSectionHome() {
-    rightSectionHome.style.display = "block";
-    rSectionSlider.style.display = "none";
-  }
-
-  if (window.innerWidth < 500) {
-    hideRightSectionHome();
-  } else {
-    showRightSectionHome();
-  }
+  updateLayout();
 
   window.addEventListener("resize", function () {
-    if (window.innerWidth < 500) {
-      hideRightSectionHome();
-    } else {
-      showRightSectionHome();
-    }
+    updateLayout();
   });
 });
--- test-slick.js
+++
@@ -1,4 +0,0 @@
- $(document).ready(function(){
-      $(".image-slider").slick({
-      });
-});
