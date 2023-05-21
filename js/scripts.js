a0686a3e15d6c0b9e14ade123281ec58a93aea14
khuong-d4ng <khuongdang.pham@gmail.com>
2023-05-21 21:25:26 +0700

    mtest ma đạo

--- js/scripts.js
+++ js/scripts.js
@@ -176,3 +176,30 @@ window.addEventListener("DOMContentLoaded", function () {
 
 //cuộn
 window.addEventListener('scroll', myScrollHandler, { passive: true });
+function wp_dereg_script_comment_reply(){wp_deregister_script( 'comment-reply' );}
+add_action('init','wp_dereg_script_comment_reply');
+
+//Function checks if a given script is already loaded
+function isScriptLoaded(src){
+  return document.querySelector('script[src="' + src + '"]') ? true : false;
+}
+
+//When a reply link is clicked, check if reply-script is loaded. If not, load it and emulate the click
+$('.comment-reply-link').click(function(){ 
+  if(!(isScriptLoaded("/wp-includes/js/comment-reply.min.js"))){
+      var script = document.createElement('script');
+      script.src = "/wp-includes/js/comment-reply.min.js"; 
+  script.onload = emRepClick($(this).attr('data-commentid'));        
+      document.head.appendChild(script);
+  } 
+});
+//Function waits 50 ms before it emulates a click on the relevant reply link now that the reply script is loaded
+function emRepClick(comId) {
+sleep(50).then(() => {
+document.querySelectorAll('[data-commentid="'+comId+'"]')[0].dispatchEvent(new Event('click'));
+});
+}
+//Function does nothing, for a given amount of time
+function sleep (time) {
+return new Promise((resolve) => setTimeout(resolve, time));
+}
