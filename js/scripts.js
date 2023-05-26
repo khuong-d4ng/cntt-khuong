8a9e0864d2f7884ff1e3b773e17d47277363db63
khuong-d4ng <khuongdang.pham@gmail.com>
2023-05-26 14:11:23 +0700

    them footer

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,35 @@
+function includeHTML() {
+  var z, i, elmnt, file, xhttp;
+  /* vong lap de lay het element trong html can` include */
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain atrribute:*/
+    file = elmnt.getAttribute("include-html");
+    if (file) {
+      /* Make an HTTP request using the attribute value as the file name: */
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function () {
+        if (this.readyState == 4) {
+          if (this.status == 200) {
+            elmnt.innerHTML = this.responseText;
+          }
+          if (this.status == 404) {
+            elmnt.innerHTML = "Page not found.";
+          }
+          /* Remove the attribute, and call this function once more: */
+          elmnt.removeAttribute("include-html");
+          includeHTML();
+        }
+      };
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      /* Exit the function: */
+      return;
+    }
+  }
+}
+
 var parentItems = document.querySelectorAll("#menu .menu-parent > li");
 var dropdownItems = document.querySelectorAll("#menu .submenu > li");
 
